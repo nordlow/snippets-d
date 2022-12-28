30d7198d5794b24376d443d02657c3cc73f5fd6a
Per Nordlöw <per.nordlow@gmail.com>
2022-12-28 09:40:42 +0100

    *** empty log message ***

--- is_static.d
+++ is_static.d
@@ -1,5 +1,8 @@
+static int m_x;
+
+static assert(is(m_x == static)); // TODO: should work
+
 @safe pure unittest {
     const int x;
-    static assert(!is(x == const));
     static assert(!is(x == static)); // TODO: should work
 }
