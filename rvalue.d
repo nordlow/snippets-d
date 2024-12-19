67c31f6b39ad5016a38731a2671e949555bb76a2
Per Nordlöw <per.nordlow@gmail.com>
2024-12-19 11:51:01 +0100

    *** empty log message ***

--- rvalue.d
+++ rvalue.d
@@ -1,11 +1,17 @@
-struct S {
-	@disable this(this);
-	int v;
-}
-
-pure @safe unittest {
+@safe unittest {
 	auto x = S(42);
+	assert(_moveCounter == 0);
 	const y = __rvalue(x);
+	assert(_moveCounter == 1); // fails as: [unittest] 0 != 1 [run]
 	assert(x.v == 42);
 	assert(y.v == 42);
 }
+
+struct S {
+	this(ref typeof(this) rhs) {
+		this.v = rhs.v;
+		_moveCounter += 1;
+	}
+	private int v;
+}
+static private size_t _moveCounter;
